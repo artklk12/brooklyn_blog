5a251b2b5be9b8ed9f44f6198081f0850403ae88
artklk12 <artem-kulik@list.ru>
2023-05-02 16:33:11 +0300

    v0.95

--- myblog/settings.py
+++ myblog/settings.py
@@ -112,14 +112,26 @@ WSGI_APPLICATION = 'myblog.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': os.getenv('DB_NAME'),
+#         'USER': os.getenv('DB_USER'),
+#         'PASSWORD': os.getenv('DB_PASSWORD'),
+#         'HOST': os.getenv('DB_HOST'),
+#         'PORT': os.getenv('DB_PORT'),
+#     },
+# }
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': 'flags',
+        'USER': 'user',
+        'PASSWORD': 'VCfyJqOKU7K64urnaUPLpB1lHxZVlEXu',
+        'HOST': 'dpg-cfjvad9mbjsn9ea1t83g-a.frankfurt-postgres.render.com',
+        'PORT': '5432',
     },
 }
 
